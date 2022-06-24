af53c5424bba56cf8fe042d182660e5b1d9d5d8f
561fffDemon <76696800+561fffDemon@users.noreply.github.com>
2022-06-24 12:28:53 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 CFG = {
-    'Bottoken': 'OTg1ODQ2MjQ3NDg1MjE0NzMw.G5XJpY.0plGc7XD7gSiY_Fu4jHW_3bMDIq5in4Qsuq0rY',
+    'Bottoken': 'OTg1ODQ2MjQ3NDg1MjE0NzMw.GA_qhY.gpkdJWT4rGwE7VlHGuQA2T5eKdCcF7rb9eHvrU',
     'Prefix': '`'
 }
